20f2965d31dd64b678ca83ca1028449b5866b8cb
akash-svg <akash001karim@gmail.com>
2021-09-27 22:32:57 +0600

    some add

--- app/src/main/java/com/example/msfpocketlist/ForgotPassword.java
+++ app/src/main/java/com/example/msfpocketlist/ForgotPassword.java
@@ -2,13 +2,27 @@ package com.example.msfpocketlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ForgotPassword extends AppCompatActivity {
+    Button nexe2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        nexe2=findViewById(R.id.btn_next_2);
+
+        nexe2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ForgotPassword.this,HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/msfpocketlist/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.msfpocketlist;
+
+import static com.example.msfpocketlist.R.layout.*;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
