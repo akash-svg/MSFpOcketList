099b9266a0873ed9c7a06b30b89839b9b71a3dd0
akash-svg <akash001karim@gmail.com>
2021-11-21 20:57:19 +0600

    some new file add

---
+++ app/src/main/java/com/example/msfpocketlist/MISSION.java
@@ -0,0 +1,21 @@
+package com.example.msfpocketlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MISSION extends AppCompatActivity {
+
+    //BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mission);
+
+        //bottomNavigationView = findViewById(R.id.bottom_View);
+        //bottomNavigationView.setBackground(null);
+    }
+}
---
+++ app/src/main/java/com/example/msfpocketlist/MSF_HQ.java
@@ -0,0 +1,14 @@
+package com.example.msfpocketlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MSF_HQ extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_msf_hq);
+    }
+}
